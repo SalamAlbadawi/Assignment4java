c6e5c85b87320f803ff418023765eba3adac213e
Salam Albadawi <alba0132@algonquinlive.com>
2023-12-06 22:15:18 -0500

    add Some notations

---
+++ REST-ACMECollege-Skeleton/src/main/java/acmecollege/entity/AcademicStudentClub_.java
@@ -0,0 +1,9 @@
+package acmecollege.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2023-07-19T00:26:56.711-0400")
+@StaticMetamodel(AcademicStudentClub.class)
+public class AcademicStudentClub_ extends StudentClub_ {
+}
---
+++ REST-ACMECollege-Skeleton/src/main/java/acmecollege/entity/NonAcademicStudentClub_.java
@@ -0,0 +1,9 @@
+package acmecollege.entity;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="Dali", date="2023-07-19T00:28:57.830-0400")
+@StaticMetamodel(NonAcademicStudentClub.class)
+public class NonAcademicStudentClub_ extends StudentClub_ {
+}
